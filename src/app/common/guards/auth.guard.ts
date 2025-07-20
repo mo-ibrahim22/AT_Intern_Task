a367d0bc6f1a7aa4603a0ad1803a6822fe616f4b
mo-ibrahim22 <mibrahimm002@gmail.com>
2025-07-20 07:57:26 +0300

    fix multi direction using tailwindcss

--- src/app/common/guards/auth.guard.ts
+++ src/app/common/guards/auth.guard.ts
@@ -1,17 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { ToasterService } from '../services/toaster.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const toaster = inject(ToasterService);
 
   if (authService.isAuthenticated()) {
     return true;
   } else {
-    toaster.show('Please sign in to access this page', 'warning');
     router.navigate(['/signin']);
     return false;
   }
